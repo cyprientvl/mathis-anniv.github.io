805a5cd08ecc817e7b7a243e916ac8be22cdfcdc
cyprien tavel <cyprien.tavel@gmail.com>
2026-04-15 17:44:06 +0200

    upload video

--- index.js
+++ index.js
@@ -81,10 +81,19 @@ function generateSlides(dataArray) {
   });
 }
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 fetch("data.json")
 .then(response => response.json())
 .then(data => {
-    generateSlides(data.data);
+    const shuffledData = shuffleArray(data.data);
+    generateSlides(shuffledData);
     slides = document.querySelectorAll(".slide");
     updateSlides();
 })
